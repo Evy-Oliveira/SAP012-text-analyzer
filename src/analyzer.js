b6bed49b4405107ebeb19ce270c4af085f1d2fe1
Evy-Oliveira <evelyn.o-s@hotmail.com>
2023-12-14 12:42:44 -0300

    css e html finalizados, inciei o JS. Este commit é para ter o arquivo atualizado no github

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,22 @@
 const analyzer = {  
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+    //TODO: Esta função deve retornar a contagem de palavras encontradas no parâmetro `text` do tipo `string`.
   },
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    //TODO: Esta função deve retornar a contagem de caracteres encontrados no parâmetro `text` do tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    //TODO: esta função deve retornar a contagem de caracteres excluindo espaços e sinais de pontuação encontrados no parâmetro `text` do tipo `string`.
   },
   getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //TODO: Esta função deve retornar o comprimento médio das palavras encontradas no parâmetro `text` do tipo `string.
   },
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    //TODO: Esta função deve retornar quantos números estão no parâmetro `text` do tipo `string`.
   },
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    //TODO: Esta função deve retornar a soma de todos os números encontrados no parâmetro `text` do tipo `string`.
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//TODO: Ouça eventos DOM e invoque métodos do objeto `analyzer`
+let botaoReset = document.getElementById("reset-button");
+
+let areaTexto = document.querySelector("textarea")
+// areaTexto.style.backgroundColor = "purple" isso é um teste para ver se funciona
+
+let liWordCount = document.querySelector("li[data-testid='word-count']")
+
+let liCharacterCount = document.querySelector("li[data-testid='character-count']")
+
+let liCharacterNoSpacesCount = document.querySelector("li[data-testid='character-no-spaces-count']")
+
+let liNumberCount = document.querySelector("li[data-testid='number-count']")
+
+let liNumberSun = document.querySelector("li[data-testid='number-sun']")
+
+let liWordLengthAverage = document.querySelector("li[data-testid='word-length-average']")
+
+botaoReset.addEventListener("click",()=>{
+    console.log('aaa')
+    areaTexto.textContent
+})
