bbcaf0a3e81349b1e11bd6c16bf2b737216c4b0b
Evy-Oliveira <evelyn.o-s@hotmail.com>
2023-12-18 16:16:42 -0300

    finalizando o analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,45 @@
 const analyzer = {  
   getWordCount: (text) => {
-
     //TODO: Esta função deve retornar a contagem de palavras encontradas no parâmetro `text` do tipo `string`.
+    return text.trim().split(' ').length
   },
   getCharacterCount: (text) => {
     //TODO: Esta função deve retornar a contagem de caracteres encontrados no parâmetro `text` do tipo `string`.
+    return text.length
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta função deve retornar a contagem de caracteres excluindo espaços e sinais de pontuação encontrados no parâmetro `text` do tipo `string`.
+    // o que esta dentro de () depois de replace tira o espaço e pontuações, se quisesse tirar só os espaços poderia usar(' ', '')/(/ /g, '')/(/\s/g,'')
+    return text.replace(/[^\w]/g,'').length
   },
   getAverageWordLength: (text) => {    
     //TODO: Esta função deve retornar o comprimento médio das palavras encontradas no parâmetro `text` do tipo `string.
+    let palavras = analyzer.getWordCount(text);
+    let caracteresSemEspaco = analyzer.getCharacterCountExcludingSpaces(text);
+    let total = (caracteresSemEspaco/palavras);
+    let media = total.toFixed(2);
+    return media
   },
   getNumberCount: (text) => {
     //TODO: Esta função deve retornar quantos números estão no parâmetro `text` do tipo `string`.
+   let numero = text.replace(/[^0-9]/g,'');
+    return parseInt(numero)
+  
   },
   getNumberSum: (text) => {
     //TODO: Esta função deve retornar a soma de todos os números encontrados no parâmetro `text` do tipo `string`.
+   //  if (!isNaN()) (parseInt(numero)) && isFinite(numero);
+    let somaNumeros = analyzer.getNumberCount(text);
+    let result = ""
+
+    for(let i = 0; i < somaNumeros.length; i++) {
+      if(somaNumeros){
+        result += somaNumeros[i] + somaNumeros[i];
+        return result;
+      }else{
+        return 0;
+      }
+    }
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import analyzer from './analyzer.js';
 
 //TODO: Ouça eventos DOM e invoque métodos do objeto `analyzer`
-let botaoReset = document.getElementById("reset-button");
+
 
 let areaTexto = document.querySelector("textarea")
 // areaTexto.style.backgroundColor = "purple" isso é um teste para ver se funciona
@@ -18,7 +18,19 @@ let liNumberSun = document.querySelector("li[data-testid='number-sun']")
 
 let liWordLengthAverage = document.querySelector("li[data-testid='word-length-average']")
 
-botaoReset.addEventListener("click",()=>{
-    console.log('aaa')
-    areaTexto.textContent
+
+document.querySelector("textarea").addEventListener("input", function(){
+    liWordCount.textContent = "Palavras: " + analyzer.getWordCount(areaTexto.value)
+    liCharacterCount.textContent = "Caracteres: " + analyzer.getCharacterCount(areaTexto.value)
+    liCharacterNoSpacesCount.textContent = "Caracteres sem espaços: " + analyzer.getCharacterCountExcludingSpaces(areaTexto.value)
+    liNumberCount.textContent = "Números: " + analyzer.getNumberCount(areaTexto.value)
+    liNumberSun.textContent = "Soma dos números: " + analyzer.getNumberSum(areaTexto.value)
+    liWordLengthAverage.textContent = "Comprimento médio das Palavras: " + analyzer.getAverageWordLength(areaTexto.value)
+    
+
+})
+
+document.getElementById("reset-button").addEventListener("click",function(){
+    areaTexto.value = ""
+//   Atrave do ID ele ta pegando o botão, o addEventListener(esta criando um observador para o botão) e sempre que clicar no botão o observador criado vai excutar a ação
 })
