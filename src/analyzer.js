ec7e22fcaf32a622a429ed4b79f89277d44d2ae8
Evy-Oliveira <evelyn.o-s@hotmail.com>
2023-12-20 09:36:43 -0300

    HTML e CSS prontos, avanço no JS

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,4 +1,4 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: Esta função deve retornar a contagem de palavras encontradas no parâmetro `text` do tipo `string`.
     return text.trim().split(' ').length
@@ -10,36 +10,36 @@ const analyzer = {
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta função deve retornar a contagem de caracteres excluindo espaços e sinais de pontuação encontrados no parâmetro `text` do tipo `string`.
     // o que esta dentro de () depois de replace tira o espaço e pontuações, se quisesse tirar só os espaços poderia usar(' ', '')/(/ /g, '')/(/\s/g,'')
-    return text.replace(/[^\w]/g,'').length
+    return text.replace(/[^\w]/g, '').length
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: Esta função deve retornar o comprimento médio das palavras encontradas no parâmetro `text` do tipo `string.
-    let palavras = analyzer.getWordCount(text);
-    let caracteresSemEspaco = analyzer.getCharacterCountExcludingSpaces(text);
-    let total = (caracteresSemEspaco/palavras);
-    let media = total.toFixed(2);
+    const palavras = analyzer.getWordCount(text);
+    const caracteresSemEspaco = analyzer.getCharacterCountExcludingSpaces(text);
+    const total = (caracteresSemEspaco / palavras);
+    const media = total.toFixed(2);
     return media
   },
   getNumberCount: (text) => {
     //TODO: Esta função deve retornar quantos números estão no parâmetro `text` do tipo `string`.
-   let numero = text.replace(/[^0-9]/g,'');
-    return parseInt(numero)
-  
+    const numero = text.replace(/[^0-9]/g, '');
+    if (numero.length > 0)
+      return numero
+    else return 0
+
   },
   getNumberSum: (text) => {
     //TODO: Esta função deve retornar a soma de todos os números encontrados no parâmetro `text` do tipo `string`.
-   //  if (!isNaN()) (parseInt(numero)) && isFinite(numero);
-    let somaNumeros = analyzer.getNumberCount(text);
-    let result = ""
 
-    for(let i = 0; i < somaNumeros.length; i++) {
-      if(somaNumeros){
-        result += somaNumeros[i] + somaNumeros[i];
-        return result;
-      }else{
-        return 0;
-      }
+    const algarismosDoTexto = analyzer.getNumberCount(text);
+    let resultado = 0;
+
+    for (let i = 0; i < algarismosDoTexto.length; i++) {
+
+      resultado += parseInt(algarismosDoTexto[i]);
+
     }
+    return resultado;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,36 +1,30 @@
 import analyzer from './analyzer.js';
 
 //TODO: Ouça eventos DOM e invoque métodos do objeto `analyzer`
+const areaTexto = document.querySelector("textarea")
 
+const liWordCount = document.querySelector("li[data-testid='word-count']")
 
-let areaTexto = document.querySelector("textarea")
-// areaTexto.style.backgroundColor = "purple" isso é um teste para ver se funciona
+const liCharacterCount = document.querySelector("li[data-testid='character-count']")
 
-let liWordCount = document.querySelector("li[data-testid='word-count']")
+const liCharacterNoSpacesCount = document.querySelector("li[data-testid='character-no-spaces-count']")
 
-let liCharacterCount = document.querySelector("li[data-testid='character-count']")
+const liNumberCount = document.querySelector("li[data-testid='number-count']")
 
-let liCharacterNoSpacesCount = document.querySelector("li[data-testid='character-no-spaces-count']")
+const liNumberSun = document.querySelector("li[data-testid='number-sun']")
 
-let liNumberCount = document.querySelector("li[data-testid='number-count']")
+const liWordLengthAverage = document.querySelector("li[data-testid='word-length-average']")
 
-let liNumberSun = document.querySelector("li[data-testid='number-sun']")
-
-let liWordLengthAverage = document.querySelector("li[data-testid='word-length-average']")
-
-
-document.querySelector("textarea").addEventListener("input", function(){
-    liWordCount.textContent = "Palavras: " + analyzer.getWordCount(areaTexto.value)
-    liCharacterCount.textContent = "Caracteres: " + analyzer.getCharacterCount(areaTexto.value)
-    liCharacterNoSpacesCount.textContent = "Caracteres sem espaços: " + analyzer.getCharacterCountExcludingSpaces(areaTexto.value)
-    liNumberCount.textContent = "Números: " + analyzer.getNumberCount(areaTexto.value)
-    liNumberSun.textContent = "Soma dos números: " + analyzer.getNumberSum(areaTexto.value)
-    liWordLengthAverage.textContent = "Comprimento médio das Palavras: " + analyzer.getAverageWordLength(areaTexto.value)
-    
-
+areaTexto.addEventListener("input", function () {
+  liWordCount.textContent = "Palavras: " + analyzer.getWordCount(areaTexto.value)
+  liCharacterCount.textContent = "Caracteres: " + analyzer.getCharacterCount(areaTexto.value)
+  liCharacterNoSpacesCount.textContent = "Caracteres sem espaços: " + analyzer.getCharacterCountExcludingSpaces(areaTexto.value)
+  liNumberCount.textContent = "Números: " + analyzer.getNumberCount(areaTexto.value)
+  liNumberSun.textContent = "Soma dos números: " + analyzer.getNumberSum(areaTexto.value)
+  liWordLengthAverage.textContent = "Comprimento médio das Palavras: " + analyzer.getAverageWordLength(areaTexto.value)
 })
 
-document.getElementById("reset-button").addEventListener("click",function(){
-    areaTexto.value = ""
-//   Atrave do ID ele ta pegando o botão, o addEventListener(esta criando um observador para o botão) e sempre que clicar no botão o observador criado vai excutar a ação
+document.getElementById("reset-button").addEventListener("click", function () {
+  areaTexto.value = ""
+//   Atraves do ID ele ta pegando o botão, o addEventListener(esta criando um observador para o botão) e sempre que clicar no botão o observador criado vai excutar a ação
 })
